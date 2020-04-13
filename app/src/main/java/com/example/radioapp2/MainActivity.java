e3d51655244f5888c22a8f9892ad6c7240093fa2
linda <q669441@gmail.com>
2020-04-13 11:50:14 +0800

    提交

--- app/src/main/java/com/example/radioapp2/MainActivity.java
+++ app/src/main/java/com/example/radioapp2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.radioapp2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void btnOk(View view) {
+        CheckBox chk;
+        TextView txvshow;
+        String msg = "";
+
+        int[] id = {R.id.chkchinese, R.id.chkenglish, R.id.chkmath, R.id.chksince};
+        for (int i : id) {
+            chk = findViewById(i);
+            if (chk.isChecked())
+                msg += "\n" + chk.getText();
+        }
+        txvshow =  findViewById(R.id.txvshow);
+        txvshow.setText(msg);
     }
 }
